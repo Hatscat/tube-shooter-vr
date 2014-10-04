d4fc5cfce2a6bdf9240311b2241ead081ada0840
Lucien Boudy <luboudy@gmail.com>
2014-10-04 15:54:37 +0200

    Vector3 tube.getPosOnTubeMesh (float theta, float z)

--- tube-shooter-vr/Assets/scripts/MathTools.cs
+++ tube-shooter-vr/Assets/scripts/MathTools.cs
@@ -3,22 +3,22 @@ using System.Collections;
 
 namespace MathTools
 {
-		public class CoordSystem
+	public class CoordSystem
+	{
+		public struct Cylindrical
 		{
-				public struct Cylindrical
-				{
-						public Cylindrical (float rho, float theta, float z)
-						{
-								this.rho = rho;
-								this.theta = theta;
-								this.z = z;
-						}
+			public Cylindrical (float rho, float theta, float z)
+			{
+				this.rho = rho;
+				this.theta = theta;
+				this.z = z;
+			}
+			
+			public float rho;
+			public float theta;
+			public float z;
+		}
 		
-						public float rho;
-						public float theta;
-						public float z;
-				}
-
 		public struct CylindricalB
 		{
 			public CylindricalB (float rho, Vector2 shift, float theta, float z)
@@ -28,7 +28,7 @@ namespace MathTools
 				this.theta = theta;
 				this.z = z;
 			}
-
+			
 			public float rho;
 			public Vector2 shift;
 			public float theta;
@@ -37,151 +37,153 @@ namespace MathTools
 		
 		public struct Spherical
 		{
-						public Spherical (float rho, float theta, float phi)
-						{
-								this.rho = rho;
-								this.theta = theta;
-								this.phi = phi;
-						}
+			public Spherical (float rho, float theta, float phi)
+			{
+				this.rho = rho;
+				this.theta = theta;
+				this.phi = phi;
+			}
+			
+			public float rho;
+			public float theta;
+			public float phi;
+		}
 		
-						public float rho;
-						public float theta;
-						public float phi;
-				}
-	
-				//Tools
-				public static Vector3 CylindricToCartesian (Cylindrical cylCoordinates)
-				{
-						return new Vector3 (cylCoordinates.rho * Mathf.Cos (cylCoordinates.theta), cylCoordinates.z, cylCoordinates.rho * Mathf.Sin (cylCoordinates.theta));
-				}
-
-				public static Vector3 CylindricToCartesianA (Cylindrical cylCoordinates)
-				{
-					return new Vector3(cylCoordinates.rho * Mathf.Cos(cylCoordinates.theta), cylCoordinates.rho * Mathf.Sin(cylCoordinates.theta), cylCoordinates.z);
-				}
-
-				public static Vector3 CylindricToCartesianB (CylindricalB cylCoordinates)
-				{
+		//Tools
+		public static Vector3 CylindricToCartesian (Cylindrical cylCoordinates)
+		{
+			return new Vector3 (cylCoordinates.rho * Mathf.Cos (cylCoordinates.theta), cylCoordinates.z, cylCoordinates.rho * Mathf.Sin (cylCoordinates.theta));
+		}
+		
+		public static Vector3 CylindricToCartesianA (Cylindrical cylCoordinates)
+		{
+			return new Vector3(cylCoordinates.rho * Mathf.Cos(cylCoordinates.theta), cylCoordinates.rho * Mathf.Sin(cylCoordinates.theta), cylCoordinates.z);
+		}
+		
+		public static Vector3 CylindricToCartesianB (CylindricalB cylCoordinates)
+		{
 			return new Vector3(cylCoordinates.rho * Mathf.Cos(cylCoordinates.theta) + cylCoordinates.shift.x, cylCoordinates.rho * Mathf.Sin(cylCoordinates.theta) + cylCoordinates.shift.y, cylCoordinates.z);
-				}
+		}
 		
 		public static Cylindrical CartesianToCylindric (Vector3 position)
-				{
-						float rho = Mathf.Sqrt (position.x * position.x + position.z * position.z);
-						float theta = 0;
-		
-						if (position.x == 0) {
-								if (position.z == 0)
-										theta = 0;
-								else if (position.z > 0)
-										theta = Mathf.PI / 2f;
-								else
-										theta = 3 * Mathf.PI / 2f;
-						} else if (position.x > 0) {
-								theta = Mathf.Atan (position.z / position.x);
-						} else {
-								theta = Mathf.PI + Mathf.Atan (position.z / position.x);
-						}
-		
-						while (theta<0)
-								theta += 2 * Mathf.PI;
-		
-						float z = position.y;
-		
-						return new Cylindrical (rho, theta, z);
-				}
-	
-				public static Vector3 SphericalToCartesian (Spherical sphCoordinates)
-				{
-						return sphCoordinates.rho * new Vector3 (Mathf.Sin (sphCoordinates.phi) * Mathf.Cos (sphCoordinates.theta),
-		                                         Mathf.Cos (sphCoordinates.phi),
-		                                         Mathf.Sin (sphCoordinates.phi) * Mathf.Sin (sphCoordinates.theta));
-				}
-	
-				public static Spherical CartesianToSpherical (Vector3 position)
-				{
-						float rho = position.magnitude;
-						float theta = 0;
-						if (position.x == 0) {
-								if (position.z == 0)
-										theta = 0;
-								else if (position.z > 0)
-										theta = Mathf.PI / 2f;
-								else
-										theta = 3 * Mathf.PI / 2f;
-						} else if (position.x > 0) {
-								theta = Mathf.Atan (position.z / position.x);
-						} else {
-								theta = Mathf.PI + Mathf.Atan (position.z / position.x);
-						}
-		
-						while (theta<0)
-								theta += 2 * Mathf.PI;
-		
-						float phi = Mathf.PI / 2f;
-						if (position.y > 0)
-								phi = Mathf.Atan (Mathf.Sqrt (position.x * position.x + position.z * position.z) / position.y);
-						else if (position.y < 0)
-								phi = Mathf.PI + Mathf.Atan (Mathf.Sqrt (position.x * position.x + position.z * position.z) / position.y);
-		
-						return new Spherical (rho, theta, phi);
-				}
-		}
-
-		public class Funch
 		{
-				public static float sh (float t)
-				{
-						return (Mathf.Exp (t) - Mathf.Exp (-t)) * .5f;
-				}
-
-				public static float ch (float t)
-				{
-						return (Mathf.Exp (t) + Mathf.Exp (-t)) * .5f;
-				}
-
-				public static float th (float t)
-				{
-						return sh (t) / ch (t);
-				}
-
-				public static float sech (float t)
-				{
-						return 1f / ch (t);
-				}
-
-				public static float cosech (float t)
-				{
-						return 1f / sh (t);
-				}
-
-				public static float argsh (float t)
-				{
-						return Mathf.Log (t + Mathf.Sqrt (t * t + 1), 2f);
-				}
-
-				public static float argch (float t)
-				{
-						if (t >= 1)
-								return Mathf.Log (t + Mathf.Sqrt (t * t - 1), 2f);
-						else
-							return float.NaN;
-				}
-
-				public static float argth (float t)
-				{
-						if (Mathf.Abs (t) < 1)
-								return .5f * Mathf.Log ((1 + t) / (1 - t), 2f);
-						else
-								return float.NaN;
-				}
-
-				public static float argcoth (float t)
-				{
-						if (Mathf.Abs (t) > 1)
-								return .5f * Mathf.Log ((1 + t) / (t - 1), 2f);
-						else
-							return float.NaN;
-				}
+			float rho = Mathf.Sqrt (position.x * position.x + position.z * position.z);
+			float theta = 0;
+			
+			if (position.x == 0) {
+				if (position.z == 0)
+					theta = 0;
+				else if (position.z > 0)
+					theta = Mathf.PI / 2f;
+				else
+					theta = 3 * Mathf.PI / 2f;
+			} else if (position.x > 0) {
+				theta = Mathf.Atan (position.z / position.x);
+			} else {
+				theta = Mathf.PI + Mathf.Atan (position.z / position.x);
+			}
+			
+			while (theta<0)
+				theta += 2 * Mathf.PI;
+			
+			float z = position.y;
+			
+			return new Cylindrical (rho, theta, z);
 		}
+		
+		public static Vector3 SphericalToCartesian (Spherical sphCoordinates)
+		{
+			return sphCoordinates.rho * new Vector3 (Mathf.Sin (sphCoordinates.phi) * Mathf.Cos (sphCoordinates.theta),
+			                                         Mathf.Cos (sphCoordinates.phi),
+			                                         Mathf.Sin (sphCoordinates.phi) * Mathf.Sin (sphCoordinates.theta));
+		}
+		
+		public static Spherical CartesianToSpherical (Vector3 position)
+		{
+			float rho = position.magnitude;
+			float theta = 0;
+			if (position.x == 0) {
+				if (position.z == 0)
+					theta = 0;
+				else if (position.z > 0)
+					theta = Mathf.PI / 2f;
+				else
+					theta = 3 * Mathf.PI / 2f;
+			} else if (position.x > 0) {
+				theta = Mathf.Atan (position.z / position.x);
+			} else {
+				theta = Mathf.PI + Mathf.Atan (position.z / position.x);
+			}
+			
+			while (theta<0)
+				theta += 2 * Mathf.PI;
+			
+			float phi = Mathf.PI / 2f;
+			if (position.y > 0)
+				phi = Mathf.Atan (Mathf.Sqrt (position.x * position.x + position.z * position.z) / position.y);
+			else if (position.y < 0)
+				phi = Mathf.PI + Mathf.Atan (Mathf.Sqrt (position.x * position.x + position.z * position.z) / position.y);
+			
+			return new Spherical (rho, theta, phi);
+		}
+	}
+	
+	
+	public class Funch
+	{
+		public static float sh (float t)
+		{
+			return (Mathf.Exp (t) - Mathf.Exp (-t)) * .5f;
+		}
+		
+		public static float ch (float t)
+		{
+			return (Mathf.Exp (t) + Mathf.Exp (-t)) * .5f;
+		}
+		
+		public static float th (float t)
+		{
+			return sh (t) / ch (t);
+		}
+		
+		public static float sech (float t)
+		{
+			return 1f / ch (t);
+		}
+		
+		public static float cosech (float t)
+		{
+			return 1f / sh (t);
+		}
+		
+		public static float argsh (float t)
+		{
+			return Mathf.Log (t + Mathf.Sqrt (t * t + 1), 2f);
+		}
+		
+		public static float argch (float t)
+		{
+			if (t >= 1)
+				return Mathf.Log (t + Mathf.Sqrt (t * t - 1), 2f);
+			else
+				return float.NaN;
+		}
+		
+		public static float argth (float t)
+		{
+			if (Mathf.Abs (t) < 1)
+				return .5f * Mathf.Log ((1 + t) / (1 - t), 2f);
+			else
+				return float.NaN;
+		}
+		
+		public static float argcoth (float t)
+		{
+			if (Mathf.Abs (t) > 1)
+				return .5f * Mathf.Log ((1 + t) / (t - 1), 2f);
+			else
+				return float.NaN;
+		}
+	}
+	
 }
--- tube-shooter-vr/Assets/scripts/tube.cs
+++ tube-shooter-vr/Assets/scripts/tube.cs
@@ -4,10 +4,30 @@ using MathTools;
 
 public class tube : MonoBehaviour {
 
+	public static int nSegmentsX;
+	public static int nSegmentsZ;
+	public static float length;
+	public static float radius;
+	public static float radius_offset;
+	public static float noise_x;
+	public static float noise_z;
+	public static float rand_seed;
+	public static float rho_shift;
+	
 	private Mesh _tube_mesh;
 	private Vector3[] _tube_vertices;
 
 	void Start () {
+
+		nSegmentsX = 50;
+		nSegmentsZ = 100;
+		length = 100f;
+		radius = 2f;
+		radius_offset = 1.3f;
+		noise_x = 5f;
+		noise_z = 5f;
+		rand_seed = Random.value;
+		rho_shift = 0.7f;
 
 		GetComponent<MeshFilter>().mesh = _tube_mesh = createTube();
 		_tube_vertices = _tube_mesh.vertices;
@@ -17,13 +37,45 @@ public class tube : MonoBehaviour {
 	
 	}
 
+	public static Vector3 getPosOnTubeMesh (float theta, float z) {
+
+		int i = (int)(theta * nSegmentsX / (Mathf.PI * 2) * (nSegmentsX + 1));
+		int j = (int)(z / length + 0.5f);
+		
+		return CoordSystem.CylindricToCartesianB(
+			new CoordSystem.CylindricalB(
+				getRho(i, nSegmentsX, j, nSegmentsZ),
+				getRhoShift(j, nSegmentsZ),
+				i * Mathf.PI * 2 / nSegmentsX,
+				((float) j / (nSegmentsZ+1)) * length)
+			);
+	}
+
+	public static int getVerticesIndex (int i, int j, int maxSegZ) {
+
+		return j + (i * maxSegZ);
+	}
+
+
+	public static float getRho (int i, int maxSegX, int j, int maxSegZ) {
+
+		maxSegX++;
+		maxSegZ++;
+
+		return radius*(radius_offset + Mathf.PerlinNoise(Mathf.Abs(Mathf.Cos((float)i/maxSegX*Mathf.PI))*noise_x + rand_seed, Mathf.Abs(Mathf.Cos((float)j/maxSegZ*Mathf.PI))*noise_z + rand_seed));
+	}
+
+	public static Vector2 getRhoShift (int z, int maxSegZ) {
+
+		maxSegZ++;
+
+		return new Vector2(Mathf.PerlinNoise((float)z/maxSegZ,rand_seed)*z*rho_shift, Mathf.PerlinNoise(rand_seed,(float)z/maxSegZ)*z*rho_shift);
+	}
+	
 	Mesh createTube () {
 		
 		Mesh mesh = new Mesh();
 		mesh.name = "tube";
-		
-		int nSegmentsX = 50;
-		int nSegmentsZ = 100;
 		
 		int nSegmentsX1 = nSegmentsX + 1;
 		int nSegmentsZ1 = nSegmentsZ + 1;
@@ -33,32 +85,25 @@ public class tube : MonoBehaviour {
 		int[] triangles = new int[nSegmentsX * nSegmentsZ * 3 * 3];
 		Vector2[] uv = new Vector2[nSegmentsX1 * nSegmentsZ1];
 		
-		float length = 100f;
-		float radius = 2f;
-		float radius_offset = 1.3f;
-		
 		//les vertices & uv
+
 		int index = 0;
-		float noise_x = 5f;
-		float noise_z = 5f;
-		float rand_seed = Random.value;
-		float shift = 0.7f;
-		
 		for (int i = 0; i < nSegmentsX1; i++) {
 			for (int j = 0; j < nSegmentsZ1; j++) {
 				
 				vertices[index] = CoordSystem.CylindricToCartesianB(
 					new CoordSystem.CylindricalB(
-					radius * (radius_offset + Mathf.PerlinNoise(Mathf.Abs(Mathf.Cos((float)i/nSegmentsX1*Mathf.PI)) * noise_x + rand_seed, Mathf.Abs(Mathf.Cos((float)j/nSegmentsZ1*Mathf.PI)) * noise_z + rand_seed)),
-					new Vector2(Mathf.PerlinNoise((float)j/nSegmentsZ1,rand_seed)*j*shift, Mathf.PerlinNoise(rand_seed,(float)j/nSegmentsZ1)*j*shift),
-					i * Mathf.PI * 2 / nSegmentsX,
-					((float) j / nSegmentsZ1) * length)
+						getRho(i, nSegmentsX, j, nSegmentsZ),
+						getRhoShift(j, nSegmentsZ),
+						i * Mathf.PI * 2 / nSegmentsX,
+						((float) j / nSegmentsZ1) * length)
 					);
 				index++;
 			}
 		}
 		
 		//les triangles	
+
 		index = 0;
 		for (int i = 0; i < nSegmentsX; i++) {
 			for (int j = 0; j < nSegmentsZ; j++) {
