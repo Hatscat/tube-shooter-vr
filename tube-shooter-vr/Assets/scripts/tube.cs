2e2e972050959df5431c41184030f86fcfe62ee8
Lucien Boudy <luboudy@gmail.com>
2014-10-04 21:44:00 +0200

    moving tube

--- tube-shooter-vr/Assets/scripts/tube.cs
+++ tube-shooter-vr/Assets/scripts/tube.cs
@@ -13,48 +13,68 @@ public class tube : MonoBehaviour {
 	public static float noise_z;
 	public static float rand_seed;
 	public static float rho_shift;
-	
-	private Mesh _tube_mesh;
-	private Vector3[] _tube_vertices;
+	public static float update_deltaTime;
+	public static Mesh _tube_mesh;
+
+	private CoordSystem.CylindricalB[] _tube_cyl_vertices;
+	private Vector2[] rhoShiftArray;
+	//private float _speed;
+	private ulong _step;
 
 	void Start () {
 
-		nSegmentsX = 50;
-		nSegmentsZ = 100;
-		length = 100f;
+		nSegmentsX = 24;
+		nSegmentsZ = 30;
+		length = 180f;
 		radius = 2f;
 		radius_offset = 1.3f;
-		noise_x = 5f;
-		noise_z = 5f;
+		noise_x = 6f;
+		noise_z = 6f;
 		rand_seed = Random.value;
-		rho_shift = 0.7f;
+		rho_shift = 0.09f;
 
+		//_speed = 20f;
+		_step = 0;
+
+		rhoShiftArray = getAllNewRhoShift((ulong)Mathf.Pow(length, 2));
 		GetComponent<MeshFilter>().mesh = _tube_mesh = createTube();
-		_tube_vertices = _tube_mesh.vertices;
 	}
 	
 	void Update () {
-	
+
+		//_update_timer -= Time.deltaTime;
+
+		//if (transform.position.z <= -length / nSegmentsZ) {
+			//_update_timer = update_deltaTime;
+			//_step++;
+			animateTube();
+			//transform.position = Vector3.zero;
+		//} else {
+			//transform.Translate(Vector3.back * _speed * Time.deltaTime);
+		//}
+		//print(getPosOnTubeMesh(Mathf.PI, 2.5f));
 	}
 
 	public static Vector3 getPosOnTubeMesh (float theta, float z) {
 
-		int i = (int)(theta * nSegmentsX / (Mathf.PI * 2) * (nSegmentsX + 1));
-		int j = (int)(z / length + 0.5f);
-		
-		return CoordSystem.CylindricToCartesianB(
-			new CoordSystem.CylindricalB(
-				getRho(i, nSegmentsX, j, nSegmentsZ),
-				getRhoShift(j, nSegmentsZ),
-				i * Mathf.PI * 2 / nSegmentsX,
-				((float) j / (nSegmentsZ+1)) * length)
-			);
+		//int i = (int)(theta * nSegmentsX / (Mathf.PI * 2) * (nSegmentsX + 1));
+		float i_f = theta / (Mathf.PI * 2) * nSegmentsX;
+		float j_f = z / length;
+
+		int[] i_s = new int[2] {(int)i_f, (int)i_f+1};
+		int[] j_s = new int[2] {(int)j_f, (int)j_f+1};
+//		Debug.Log(i_s[0]);
+//		Debug.Log(i_s[1]);
+//		Debug.Log(j_s[0]);
+//		Debug.Log(j_s[1]);
+		return Vector3.Lerp(_tube_mesh.vertices[j_s[0]+(i_s[1]*nSegmentsZ)], _tube_mesh.vertices[j_s[1]+(i_s[0]*nSegmentsZ)], (i_f-i_s[0])*(j_f-j_s[0]));
+		//return Vector3.zero;
 	}
 
-	public static int getVerticesIndex (int i, int j, int maxSegZ) {
-
-		return j + (i * maxSegZ);
-	}
+//	public static int getVerticesIndex (int i, int j, int maxSegZ) {
+//
+//		return j + (i * maxSegZ);
+//	}
 
 
 	public static float getRho (int i, int maxSegX, int j, int maxSegZ) {
@@ -65,11 +85,19 @@ public class tube : MonoBehaviour {
 		return radius*(radius_offset + Mathf.PerlinNoise(Mathf.Abs(Mathf.Cos((float)i/maxSegX*Mathf.PI))*noise_x + rand_seed, Mathf.Abs(Mathf.Cos((float)j/maxSegZ*Mathf.PI))*noise_z + rand_seed));
 	}
 
-	public static Vector2 getRhoShift (int z, int maxSegZ) {
+	public static Vector2 getRhoShift (int j, int maxSegZ) {
 
 		maxSegZ++;
 
-		return new Vector2(Mathf.PerlinNoise((float)z/maxSegZ,rand_seed)*z*rho_shift, Mathf.PerlinNoise(rand_seed,(float)z/maxSegZ)*z*rho_shift);
+		return new Vector2(Mathf.PerlinNoise((float)j/maxSegZ,rand_seed)*rho_shift, Mathf.PerlinNoise(rand_seed,(float)j/maxSegZ)*rho_shift).normalized * j;
+	}
+
+	public static Vector2[] getAllNewRhoShift (ulong l) {
+		Vector2[] rhoShiftArray = new Vector2[l];
+		for (ulong i = 0; i < l; i++) {
+			rhoShiftArray[i] = new Vector2((Mathf.PerlinNoise((float)i/99,rand_seed)*2-1)*rho_shift, (Mathf.PerlinNoise(rand_seed,(float)i/99)*2-1)*rho_shift);
+		}
+		return rhoShiftArray;
 	}
 	
 	Mesh createTube () {
@@ -81,6 +109,7 @@ public class tube : MonoBehaviour {
 		int nSegmentsZ1 = nSegmentsZ + 1;
 		
 		Vector3[] vertices = new Vector3[nSegmentsX1 * nSegmentsZ1];
+		_tube_cyl_vertices = new CoordSystem.CylindricalB[nSegmentsX1 * nSegmentsZ1];
 		//Vector3[] normals = new Vector3[3];
 		int[] triangles = new int[nSegmentsX * nSegmentsZ * 3 * 3];
 		Vector2[] uv = new Vector2[nSegmentsX1 * nSegmentsZ1];
@@ -90,14 +119,14 @@ public class tube : MonoBehaviour {
 		int index = 0;
 		for (int i = 0; i < nSegmentsX1; i++) {
 			for (int j = 0; j < nSegmentsZ1; j++) {
-				
-				vertices[index] = CoordSystem.CylindricToCartesianB(
-					new CoordSystem.CylindricalB(
-						getRho(i, nSegmentsX, j, nSegmentsZ),
-						getRhoShift(j, nSegmentsZ),
-						i * Mathf.PI * 2 / nSegmentsX,
-						((float) j / nSegmentsZ1) * length)
-					);
+
+				_tube_cyl_vertices[index] = new CoordSystem.CylindricalB(
+					getRho(i, nSegmentsX, j, nSegmentsZ),
+					Vector2.zero,
+					i * Mathf.PI * 2 / nSegmentsX,
+					((float) j / nSegmentsZ1) * length);
+
+				vertices[index] = CoordSystem.CylindricToCartesianB(_tube_cyl_vertices[index]);
 				index++;
 			}
 		}
@@ -138,4 +167,37 @@ public class tube : MonoBehaviour {
 		
 		return mesh;
 	}
+
+	void animateTube () {
+
+		int nSegmentsX1 = nSegmentsX + 1;
+		int nSegmentsZ1 = nSegmentsZ + 1;
+
+		Vector3[] _tube_new_vertices = new Vector3[_tube_mesh.vertices.Length];
+		CoordSystem.CylindricalB[] _tube_new_cyl_vertices = new CoordSystem.CylindricalB[_tube_cyl_vertices.Length];
+
+		_step = _step < (ulong)rhoShiftArray.Length -1 ? _step + 1 : 0;
+
+		int index = 0;
+		for (int i = 0; i < nSegmentsX1; i++) {
+			for (int j = 0; j < nSegmentsZ1; j++) {
+
+				_tube_new_cyl_vertices[index] = new CoordSystem.CylindricalB(
+					_tube_cyl_vertices[(index + 1) % _tube_mesh.vertices.Length].rho,
+					rhoShiftArray[(_step + (ulong)j) % (ulong)rhoShiftArray.Length] * j * j,
+					i * Mathf.PI * 2 / nSegmentsX,
+					((float) j / nSegmentsZ1) * length);
+					
+				_tube_new_vertices[index] = CoordSystem.CylindricToCartesianB(_tube_new_cyl_vertices[index]);
+				index++;
+			}
+		}
+
+		_tube_cyl_vertices = _tube_new_cyl_vertices.Clone() as CoordSystem.CylindricalB[];
+		_tube_mesh.vertices = _tube_new_vertices.Clone() as Vector3[];
+		_tube_mesh.RecalculateBounds();
+		//GetComponent<MeshFilter>().mesh
+		//_tube_mesh.RecalculateNormals();
+	}
+
 }
